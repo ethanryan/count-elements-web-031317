de5e5aaa488450fce17cdf496a5f7dad1eb1d4a2
ethanryan <ethanryan@gmail.com>
2017-03-21 22:34:31 -0400

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
-end
- 
+  new_hash = {}
+  array.each do |key, value|
+    if new_hash.include?(key) #if key already in hash...
+      new_hash[key] += 1  #add 1 to that key's value
+    else                  #otherwise...
+      new_hash[key] = 1   #create that key and set it's value to 1
+    end #end if statement
+  end #end loop
+  new_hash
+end #end method
